3b72e868890d238c96c89b07b213925d4c7be9da
Vladimír Pokorný <me@JustLeader.eu>
2021-12-21 13:06:52 +0100

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,12 +2,12 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'JLMC'
+copyright = '2021, JustLeaderův Minecraft Server'
+author = 'JustLeader'
 
 release = '0.1'
-version = '0.1.0'
+version = '0.1.1'
 
 # -- General configuration
 
